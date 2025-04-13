87d3ce91a47226af0b47e52dcbb90eb1651dc1ec
Daly <dalydevelopment15@gmail.com>
2025-04-13 15:46:34 +0200

    Add asset management and update dependencies
    - Created a new file `app_images.dart` to manage image assets with constants for easy access.
    - Updated `pubspec.yaml` to include `flutter_native_splash` and `flutter_launcher_icons` dependencies.
    - Configured `flutter_icons` and `flutter_native_splash` settings in `pubspec.yaml`.
    - Added asset paths for images and fonts in the Flutter configuration.
    - Updated `pubspec.lock` with new dependencies and their versions.

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,92 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for imagesAvatar
+  /// assets/images/avatar.jpg
+  static const String imagesAvatar = "assets/images/avatar.jpg";
+
+  /// Assets for imagesBasket
+  /// assets/images/basket.png
+  static const String imagesBasket = "assets/images/basket.png";
+
+  /// Assets for imagesBigMoney
+  /// assets/images/bigMoney.svg
+  static const String imagesBigMoney = "assets/images/bigMoney.svg";
+
+  /// Assets for imagesCartoon
+  /// assets/images/cartoon.png
+  static const String imagesCartoon = "assets/images/cartoon.png";
+
+  /// Assets for imagesDart
+  /// assets/images/dart.svg
+  static const String imagesDart = "assets/images/dart.svg";
+
+  /// Assets for imagesElectronics
+  /// assets/images/electronics.png
+  static const String imagesElectronics = "assets/images/electronics.png";
+
+  /// Assets for imagesGift
+  /// assets/images/gift.svg
+  static const String imagesGift = "assets/images/gift.svg";
+
+  /// Assets for imagesGym
+  /// assets/images/gym.png
+  static const String imagesGym = "assets/images/gym.png";
+
+  /// Assets for imagesHomeDevices
+  /// assets/images/homeDevices.png
+  static const String imagesHomeDevices = "assets/images/homeDevices.png";
+
+  /// Assets for imagesMetail
+  /// assets/images/metail.png
+  static const String imagesMetail = "assets/images/metail.png";
+
+  /// Assets for imagesMoney
+  /// assets/images/money.svg
+  static const String imagesMoney = "assets/images/money.svg";
+
+  /// Assets for imagesOil
+  /// assets/images/oil.png
+  static const String imagesOil = "assets/images/oil.png";
+
+  /// Assets for imagesPaper
+  /// assets/images/paper.png
+  static const String imagesPaper = "assets/images/paper.png";
+
+  /// Assets for imagesPlastic
+  /// assets/images/plastic.png
+  static const String imagesPlastic = "assets/images/plastic.png";
+
+  /// Assets for imagesQr
+  /// assets/images/qr.png
+  static const String imagesQr = "assets/images/qr.png";
+
+  /// Assets for imagesRecycle
+  /// assets/images/recycle.svg
+  static const String imagesRecycle = "assets/images/recycle.svg";
+
+  /// Assets for imagesRobapecia
+  /// assets/images/Robapecia.png
+  static const String imagesRobapecia = "assets/images/Robapecia.png";
+
+  /// Assets for imagesScrap
+  /// assets/images/scrap.png
+  static const String imagesScrap = "assets/images/scrap.png";
+
+  /// Assets for imagesSoldScrap
+  /// assets/images/sold_scrap.svg
+  static const String imagesSoldScrap = "assets/images/sold_scrap.svg";
+
+  /// Assets for imagesStock
+  /// assets/images/stock.svg
+  static const String imagesStock = "assets/images/stock.svg";
+
+  /// Assets for imagesToys
+  /// assets/images/toys.png
+  static const String imagesToys = "assets/images/toys.png";
+
+  /// Assets for imagesYourMoney
+  /// assets/images/yourMoney.svg
+  static const String imagesYourMoney = "assets/images/yourMoney.svg";
+}
